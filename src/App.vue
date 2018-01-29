3ba5537506db077d44dfd77cedb4f33a503c9644
Patrick Biffle <pbiffle@gmail.com>
2018-01-29 10:21:25 -0700

    completed dinosaursv2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Header></Header>
+    <main>
+      <h2>Profiles</h2>
+        <ul>
+          <Profile v-for='dinosaur in dinosaursData' v-bind:dinosaur='dinosaur' :key='dinosaur.name'></Profile>
+        </ul>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
+import Profile from './components/Profile'
+import SkillList from './components/SkillList'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      dinosaursData: [],
+      apiUrl:'../static/dinosaurs.json'
+    }
+  },
   components: {
-    HelloWorld
+    Header,
+    Profile,
+    SkillList,
+    Footer
+  },
+  mounted() {
+    fetch(this.apiUrl)
+    .then(response => response.json())
+    .then(response => {
+      this.dinosaursData = response
+    })
   }
-}
-</script>
+}</script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html {
+	margin: 0;
+	padding: 0;
+}
+
+ul{
+  list-style: none;
+}
+
+body {
+	margin: 0 30px 0 30px;
+	padding: 0;
+	font-family: sans-serif;
+	color: #1b997a;
+	display: grid;
+	grid-template-rows: 15% 75% 10%;
+}
+
+main {
+  grid-row: 2/3;
+}
+
+main section {
+  padding: 0 20px 0 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, x100%);
+  grid-gap: 10px;
+  margin: 0 auto;
+  width: 50%;
+}
+
+small {
+  color: black;
+  font-size: .5rem;
+  margin-left: 10px;
+}
+
+p {
+  font-size: .8rem;
 }
 </style>
---
+++ src/components/SkillList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="skills-list">
+    <section class="skills-container hidden">
+      <h4>Skills</h4>
+      <ul class="skills-list">
+          <li v-for='skill in dinosaur.skills'>{{skill}}</li>
+      </ul>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "SkillList",
+  props: {
+    dinosaur:{
+      type: Object,
+      required: true
+    }
+  }
+}</script>
+<style scoped>
+.skills-list {
+  list-style: none;
+}
+</style>
